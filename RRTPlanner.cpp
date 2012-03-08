7d1d2917e2754406a5dd4c90f206c2b986207641
Stefan Larsen <skytthe@gmail.com>
2012-03-09 00:24:12 +0100

    RRTplanner files

---
+++ RRTPlanner.cpp
@@ -0,0 +1,17 @@
+/*
+ * RRTPlanner.cpp
+ *
+ *  Created on: Mar 8, 2012
+ *      Author: skytthe
+ */
+
+#include "RRTPlanner.h"
+
+RRTPlanner::RRTPlanner() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RRTPlanner::~RRTPlanner() {
+	// TODO Auto-generated destructor stub
+}
---
+++ RRTPlanner.h
@@ -0,0 +1,17 @@
+/*
+ * RRTPlanner.h
+ *
+ *  Created on: Mar 8, 2012
+ *      Author: skytthe
+ */
+
+#ifndef RRTPLANNER_H_
+#define RRTPLANNER_H_
+
+class RRTPlanner {
+public:
+	RRTPlanner();
+	virtual ~RRTPlanner();
+};
+
+#endif /* RRTPLANNER_H_ */
